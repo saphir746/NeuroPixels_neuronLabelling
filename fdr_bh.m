6bfa72b408f35f7fb7def6ad062b99030d0a1640
saphir746 <deborah.schneider-luftman@crick.ac.uk>
2021-09-20 17:39:42 +0100

    On branch main  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	typechange: fdr_bh.m

--- fdr_bh.m
+++ fdr_bh.m
@@ -1,1 +1,158 @@
-/camp/stp/babs/working/schneid/projects/schaefera/julia.harris/Sniff_EEG_mice/bin/fdr_bh.m
+%   Groppe, D.M., Urbach, T.P., & Kutas, M. (2011) Mass univariate analysis 
+% of event-related brain potentials/fields I: A critical tutorial review. 
+% Psychophysiology, 48(12) pp. 1711-1725, DOI: 10.1111/j.1469-8986.2011.01273.x 
+% http://www.cogsci.ucsd.edu/~dgroppe/PUBLICATIONS/mass_uni_preprint1.pdf
+%
+%
+% Author:
+% David M. Groppe
+% Kutaslab
+% Dept. of Cognitive Science
+% University of California, San Diego
+% March 24, 2010
+function [h, crit_p, adj_p]=fdr_bh(pvals,q,method)
+% Required Input:
+%   pvals - A vector or matrix (two dimensions or more) containing the
+%           p-value of each individual test in a family of tests.
+%
+% Optional Inputs:
+%   q       - The desired false discovery rate. {default: 0.05}
+%   method  - ['pdep' or 'dep'] If 'pdep,' the original Bejnamini & Hochberg
+%             FDR procedure is used, which is guaranteed to be accurate if
+%             the individual tests are independent or positively dependent
+%             (e.g., Gaussian variables that are positively correlated or
+%             independent).  If 'dep,' the FDR procedure
+%             described in Benjamini & Yekutieli (2001) that is guaranteed
+%             to be accurate for any test dependency structure (e.g.,
+%             Gaussian variables with any covariance matrix) is used. 'dep'
+%             is always appropriate to use but is less powerful than 'pdep.'
+%             {default: 'pdep'}
+%   report  - ['yes' or 'no'] If 'yes', a brief summary of FDR results are
+%             output to the MATLAB command line {default: 'no'}
+%
+%
+% Outputs:
+%   h       - A binary vector or matrix of the same size as the input "pvals."
+%             If the ith element of h is 1, then the test that produced the 
+%             ith p-value in pvals is significant (i.e., the null hypothesis
+%             of the test is rejected).
+%   crit_p  - All uncorrected p-values less than or equal to crit_p are 
+%             significant (i.e., their null hypotheses are rejected).  If 
+%             no p-values are significant, crit_p=0.
+%   adj_ci_cvrg - The FCR BH- or BY-adjusted selected 
+%             confidence interval coverage. For any p-values that 
+%             are significant after FDR adjustment, this gives you the
+%             proportion of coverage (e.g., 0.99) you should use when generating
+%             confidence intervals for those parameters. In other words,
+%             this allows you to correct your confidence intervals for
+%             multiple comparisons. You can NOT obtain confidence intervals 
+%             for non-significant p-values. The adjusted confidence intervals
+%             guarantee that the expected FCR is less than or equal to q
+%             if using the appropriate FDR control algorithm for the  
+%             dependency structure of your data (Benjamini & Yekutieli, 2005).
+%             FCR (i.e., false coverage-statement rate) is the proportion 
+%             of confidence intervals you construct
+%             that miss the true value of the parameter. adj_ci=NaN if no
+%             p-values are significant after adjustment.
+%   adj_p   - All adjusted p-values less than or equal to q are significant
+%             (i.e., their null hypotheses are rejected). Note, adjusted 
+%             p-values can be greater than 1.
+%
+
+report='no';
+if nargin<1
+    error('You need to provide a vector or matrix of p-values.');
+else
+    if ~isempty(find(pvals<0,1))
+        error('Some p-values are less than 0.');
+    elseif ~isempty(find(pvals>1,1))
+        error('Some p-values are greater than 1.');
+    end
+end
+
+if nargin<2
+    q=.05;
+end
+
+if nargin<3
+    method='pdep';
+end
+
+%{
+if nargin<4
+    report='no';
+end
+%}
+
+s=size(pvals);
+if (length(s)>2) || s(1)>1
+    [p_sorted, sort_ids]=sort(reshape(pvals,1,prod(s)));
+else
+    %p-values are already a row vector
+    [p_sorted, sort_ids]=sort(pvals);
+end
+[dummy, unsort_ids]=sort(sort_ids); %indexes to return p_sorted to pvals order
+m=length(p_sorted); %number of tests
+
+if strcmpi(method,'pdep')
+    %BH procedure for independence or positive dependence
+    thresh=(1:m)*q/m;
+    wtd_p=m*p_sorted./(1:m);
+    
+elseif strcmpi(method,'dep')
+    %BH procedure for any dependency structure
+    denom=m*sum(1./(1:m));
+    thresh=(1:m)*q/denom;
+    wtd_p=denom*p_sorted./[1:m];
+    %Note, it can produce adjusted p-values greater than 1!
+    %compute adjusted p-values
+else
+    error('Argument ''method'' needs to be ''pdep'' or ''dep''.');
+end
+
+if nargout>2
+    %compute adjusted p-values; This can be a bit computationally intensive
+    adj_p=zeros(1,m)*NaN;
+    [wtd_p_sorted, wtd_p_sindex] = sort( wtd_p );
+    nextfill = 1;
+    for k = 1 : m
+        if wtd_p_sindex(k)>=nextfill
+            adj_p(nextfill:wtd_p_sindex(k)) = wtd_p_sorted(k);
+            nextfill = wtd_p_sindex(k)+1;
+            if nextfill>m
+                break;
+            end
+        end
+    end
+    adj_p=reshape(adj_p(unsort_ids),s);
+end
+
+rej=p_sorted<=thresh;
+max_id=find(rej,1,'last'); %find greatest significant pvalue
+if isempty(max_id)
+    crit_p=0;
+    h=pvals*0;
+    adj_ci_cvrg=NaN;
+else
+    crit_p=p_sorted(max_id);
+    h=pvals<=crit_p;
+    adj_ci_cvrg=1-thresh(max_id);
+end
+
+if strcmpi(report,'yes')
+    n_sig=sum(p_sorted<=crit_p);
+    if n_sig==1
+        fprintf('Out of %d tests, %d is significant using a false discovery rate of %f.\n',m,n_sig,q);
+    else
+        fprintf('Out of %d tests, %d are significant using a false discovery rate of %f.\n',m,n_sig,q);
+    end
+    if strcmpi(method,'pdep')
+        fprintf('FDR/FCR procedure used is guaranteed valid for independent or positively dependent tests.\n');
+    else
+        fprintf('FDR/FCR procedure used is guaranteed valid for independent or dependent tests.\n');
+    end
+end
+
+
+
+
